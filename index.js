03d2050287cf3c6c84aaeedb9a3e30bf12bb5764
mak3rs <mak3rs@mak3rss-MacBook-Pro.local>
2017-11-13 18:03:05 +0100

    stage 1

--- index.js
+++ index.js
@@ -6,7 +6,9 @@
 const Part = require('./custom_modules/part');
 //const Story = require('./custom_modules/story');
 
-console.log(process.env.IS_ENV_WORKING);
+if (process.env.IS_ENV_WORKING) {
+	console.log('Hi there!');
+}
 
 var part = new Part();
 part.initTelegram();
